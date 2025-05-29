a1a9adc0311aa94be33d3ab836a4188ca4b39341
Deepanjali <info.deepanjli@gmail.com>
2025-05-29 20:16:58 +0530

    Learn to implement class

---
+++ MessagePrinter.java
@@ -0,0 +1,13 @@
+public class MessagePrinter {
+  
+    // Method to print the message
+    public void printMessage(String name) {
+        System.out.println("hello " + name);
+    }
+ 
+ 
+    public static void main(String[] args) {
+        MessagePrinter m = new MessagePrinter();
+        m.printMessage("Deepa");
+    }
+ }
